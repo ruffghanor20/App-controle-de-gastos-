966a9d9ed82b35aecd92845de583e54b143a44a6
ruffghanor20 <133601363+ruffghanor20@users.noreply.github.com>
2026-03-03 01:40:40 +0000

    settings.gradle #definir nome do projeto

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SalaoFinanceiroNative" # Define o nome do projeto aqui
+include(":app")
